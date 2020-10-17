89d10b8c905893844e5091ae499226e8857b7d35
Kesha <kesha.tkachenko2017@mail.ru>
2020-10-18 00:15:36 +0300

    init

---
+++ FrameworkTestGHActions/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrameworkTestGHActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello world!";
+            return (IActionResult)View("Index");
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using FrameworkTestGHActions.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void IndexViewDataMessage()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Hello world!", result?.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexViewNameEqualIndex()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.AreEqual("Index", result?.ViewName);
+        }
+
+
+    }
+}
